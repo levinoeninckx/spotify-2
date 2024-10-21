87c8c4519d44c47f2f08c48c61883fa84fc03923
levi <levi@home.Home>
2024-10-21 14:36:30 +0100

    add useeffect

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,10 @@
 import { getProviders, signIn } from 'next-auth/react'
+import { useEffect } from 'react'
 
 function Login({ providers }) {
+  useEffect(async (_) => {
+    providers = await getProviders()
+  })
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
       <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />
